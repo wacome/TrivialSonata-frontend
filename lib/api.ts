68dd18908ab0d03ce4a8bfc621fbc788c8a70fea
wacome <1319332366@qq.com>
2026-04-27 00:12:37 +0800

    调整BASE_URL

--- lib/api.ts
+++ lib/api.ts
@@ -1,4 +1,4 @@
-const BASE_URL = 'http://localhost:8090/api';
+const BASE_URL = process.env.NEXT_PUBLIC_API_BASE_URL || 'http://localhost:8090/api';
 import { getUserId } from './auth';
 
 const handleResponse = async (res: Response) => {
